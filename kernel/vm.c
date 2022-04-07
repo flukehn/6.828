87d2b9be886345a568daf4057ff85a121d09d52a
flukehn <flukehn@gmail.com>
2022-04-07 21:49:31 +0800

    pte printout

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,25 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void vmprint_dfs(pagetable_t pagetable, int deep) {
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) /*&& (pte & (PTE_R|PTE_W|PTE_X)) == 0*/){
+      uint64 child = PTE2PA(pte);
+      for(int j=0;j<deep;++j){
+        printf("..");
+        if(j<deep-1) printf(" ");
+        else printf("%d: pte %p pa %p\n",i,pte,child);
+      }
+      if(deep<3)vmprint_dfs((pagetable_t)child, deep + 1);
+    } else if(pte & PTE_V){
+//      panic("vmprint: leaf");
+    }
+  }
+}
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprint_dfs(pagetable, 1);
+}
